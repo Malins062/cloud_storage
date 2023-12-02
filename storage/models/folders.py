321a50ed80ab2ca37b870f4d1ecb856cfd851642
Malins062 <6328673@gmail.com>
2023-12-02 09:37:39 +0300

    feat: added model Folder

--- storage/models/folders.py
+++ storage/models/folders.py
@@ -1,8 +1,19 @@
+import os
+from django.contrib.auth import get_user_model
+from django.db import models
+from datetime import datetime
+
+# User
+User = get_user_model()
+
 
 class Folder(models.Model):
     id = models.AutoField(unique=True, primary_key=True)
-    creator = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Владелец')
+    owner = models.ForeignKey(
+        to=User, on_delete=models.CASCADE, related_name='folders', verbose_name='Владелец'
+    )
     created_at = models.DateTimeField(default=datetime.now, verbose_name='Дата создания')
+    update_at = models.DateTimeField(default=datetime.now, verbose_name='Дата изменения')
     name = models.CharField(max_length=255, verbose_name='Папка')
     parent_id = models.IntegerField(verbose_name='Родительский каталог', blank=True, null=True)
     # url = models.SlugField(max_length=160, unique=True)
