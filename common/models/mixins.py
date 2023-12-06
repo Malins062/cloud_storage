8a6f074b1cf8d534bdc3a80321a7f0763fe8c543
Alexei Malinovskii <6328673@gmail.com>
2023-12-06 21:27:01 +0300

    feat: added abstract models

--- common/models/mixins.py
+++ common/models/mixins.py
@@ -1,15 +1,55 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-from datetime import datetime
-
+from django.utils import timezone
 
 # User
 User = get_user_model()
 
 
-class BaseInstanceStorageModel(models.Model):
-    created_at = models.DateTimeField(default=datetime.now, verbose_name='Дата создания')
-    updated_at = models.DateTimeField(default=datetime.now, verbose_name='Дата изменения')
+class DateModelMixin(models.Model):
+    created_at = models.DateTimeField(verbose_name='Дата создания', null=True, blank=True, default=None, )
+    updated_at = models.DateTimeField(verbose_name='Дата изменения', null=True, blank=True, default=None, )
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        if not self.pk and not self.created_at:
+            self.created_at = timezone.now()
+        self.updated_at = timezone.now()
+        return super(DateModelMixin, self).save(*args, **kwargs)
+
+
+class InfoModelMixin(DateModelMixin):
+    created_by = models.ForeignKey(
+        User, models.SET_NULL,
+        related_name='created_%(app_label)s_%(class)s',
+        verbose_name='Кем создано', null=True, )
+    updated_by = models.ForeignKey(
+        User, models.SET_NULL,
+        related_name='updated_%(app_label)s_%(class)s',
+        verbose_name='Кем изменено', null=True, )
+    name = models.CharField(verbose_name='Наименование', max_length=255, )
+    description = models.TextField(verbose_name='Описание', max_length=255, blank=True, default=None, )
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        from crum import get_current_user
+
+        user = get_current_user()
+        if user and not user.pk:
+            user = None
+        if not self.pk:
+            self.created_by = user
+        self.updated_by = user
+        return super().save(*args, **kwargs)
+
+
+class DescriptionModelMixin(models.Model):
+    name = models.CharField(verbose_name='Наименование', max_length=255, )
+    description = models.TextField(verbose_name='Описание', max_length=255, blank=True, default=None, )
 
     class Meta:
         abstract = True
