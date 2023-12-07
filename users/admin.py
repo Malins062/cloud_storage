71efa9ee92d1341a9833b8a77c9d6e07003ed858
Alexei Malinovskii <6328673@gmail.com>
2023-12-07 17:54:48 +0300

    feat: added new fields

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.utils.translation import gettext_lazy as _
 
 from users.models.users import User
 
@@ -9,13 +8,13 @@ from users.models.users import User
 class UserAdmin(UserAdmin):
     change_user_password_template = None
     fieldsets = (
-        (None, {'fields': ('phone_number', 'email', 'username')}),
-        (_('Личная информация'),
-         {'fields': ('first_name', 'last_name',)}),
-        (_('Permissions'), {
-            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
-        }),
-        # (_('Important dates'), {'fields': ('last_login',)}),
+        ('Идентификаторы пользователя', {'fields': ('phone_number', 'email', 'username',)}),
+        ('Личная информация', {'fields': ('first_name', 'last_name',)}),
+        ('Параметры доступа', {'fields': ('is_active', 'is_staff', 'is_superuser',)}),
+        # ('Параметры доступа', {
+        #     'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        # }),
+        ('Важные даты', {'fields': ('date_joined', 'last_login',)}),
     )
     # add_fieldsets = (
     #     (None, {
@@ -27,7 +26,7 @@ class UserAdmin(UserAdmin):
 
     list_display_links = ('id', 'username', 'full_name',)
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
-    search_fields = ('id', 'username', 'first_name', 'last_name', 'email', 'phone_number',)
-    ordering = ('-id',)
-    # filter_horizontal = ('groups', 'user_permissions',)
-    # readonly_fields = ('last_login',)
+    search_fields = ('username', 'first_name', 'last_name', 'email', 'phone_number',)
+    ordering = ('id',)
+    filter_horizontal = ('groups', 'user_permissions',)
+    readonly_fields = ('last_login', 'date_joined')
