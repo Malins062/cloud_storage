41ccb39a2b77c4aecdaf923d0dcb68807b5a08a6
Alexei Malinovskii <6328673@gmail.com>
2023-12-08 17:46:17 +0300

    fix: remove phone_number

--- users/models/managers.py
+++ users/models/managers.py
@@ -5,37 +5,29 @@ from rest_framework.exceptions import ParseError
 class CustomUserManager(BaseUserManager):
     use_in_migrations = True
 
-    def _create_user(self, phone_number=None, email=None, password=None, username=None, **extra_fields):
-        if not (email or phone_number or username):
-            raise ParseError('Укажите никнейм, адрес электронной почты или телефон!')
+    def _create_user(self, email=None, password=None, username=None, **extra_fields):
+        if not email:
+            raise ParseError('Укажите адрес электронной почты!')
 
-        if email:
-            email = self.normalize_email(email)
+        email = self.normalize_email(email)
 
         if not username:
-            if email:
-                username = email
-            else:
-                username = phone_number
+            username = email
 
         user = self.model(username=username, **extra_fields)
-        if email:
-            user.email = email
-        if phone_number:
-            user.phone_number = phone_number
-
+        user.email = email
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, phone_number=None, email=None, password=None, username=None, **extra_fields):
+    def create_user(self, email=None, password=None, username=None, **extra_fields):
         extra_fields.setdefault('is_superuser', False)
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_active', True)
-        return self._create_user(phone_number, email, password, username, **extra_fields)
+        return self._create_user(email, password, username, **extra_fields)
 
-    def create_superuser(self, phone_number=None,  email=None, password=None, username=None, **extra_fields):
+    def create_superuser(self, email=None, password=None, username=None, **extra_fields):
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_active', True)
-        return self._create_user(phone_number, email, password, username, **extra_fields)
+        return self._create_user(email, password, username, **extra_fields)
